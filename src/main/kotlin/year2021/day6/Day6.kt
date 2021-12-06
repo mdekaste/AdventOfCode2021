0d2aed04fc44600512f0c4dea83842824ffe5e94
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2021-12-06 10:32:08 +0100

    Day6 made smaller and general

--- src/main/kotlin/year2021/day6/Day6.kt
+++ src/main/kotlin/year2021/day6/Day6.kt
@@ -1,39 +1,28 @@
 package year2021.day6
 
 import Challenge
-import java.util.*
+import java.util.Collections.rotate
 
-fun main(){
+fun main() {
     Day6.printSolutions()
 }
+
 object Day6 : Challenge("--- Day 6: Lanternfish ---") {
     val parsed = input.split(",").map(String::toInt)
 
-    override fun part1(): Any? {
-        val fishCount = parsed.groupingBy { it }.eachCount().let { MutableList(7){ index -> it[index] ?: 0} }
-        var fish8 = 0
-        var fish7 = 0
-        repeat(80 + 2){
-            Collections.rotate(fishCount, - 1)
-            val temp = fishCount[6]
-            fishCount[6] += fish7
-            fish7 = fish8
-            fish8 = temp
-        }
-        return fishCount.sum()
-    }
+    override fun part1() = solve(80)
 
-    override fun part2(): Any? {
-        val fishCount = parsed.groupingBy { it }.eachCount().let { MutableList(7){ index -> it[index]?.toLong() ?: 0L} }
-        var fish8 = 0L
-        var fish7 = 0L
-        repeat(256 + 2){
-            Collections.rotate(fishCount, - 1)
-            val temp = fishCount[6]
-            fishCount[6] += fish7
-            fish7 = fish8
-            fish8 = temp
+    override fun part2() = solve(256)
+
+    fun solve(days: Int): Long = parsed
+        .groupingBy { it }
+        .eachCount()
+        .let { MutableList(9) { index -> it[index]?.toLong() ?: 0L } }
+        .apply {
+            repeat(days) {
+                rotate(this, -1)
+                this[6] += this[8]
+            }
         }
-        return fishCount.sum()
-    }
+        .sum()
 }
