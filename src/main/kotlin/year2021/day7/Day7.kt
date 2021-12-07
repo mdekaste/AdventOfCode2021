44e0a6b5bb7db1961b9eac072cb2ace370398702
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2021-12-07 09:45:00 +0100

    Day7 prettified

--- src/main/kotlin/year2021/day7/Day7.kt
+++ src/main/kotlin/year2021/day7/Day7.kt
@@ -1,7 +1,7 @@
 package year2021.day7
 
 import Challenge
-import kotlin.math.absoluteValue
+import kotlin.math.abs
 
 fun main() {
     Day7.printSolutions()
@@ -9,17 +9,12 @@ fun main() {
 
 object Day7 : Challenge("--- Day 7: The Treachery of Whales ---") {
     val parsed = input.split(",").map(String::toInt).sorted()
-    val rangeToCheck = parsed.first()..parsed.last()
+    private val rangeToCheck = parsed.first()..parsed.last()
 
-    override fun part1(): Any? {
-        return rangeToCheck.minOf { candidate ->
-            parsed.map { it - candidate }.sumOf(Int::absoluteValue)
-        }
-    }
+    override fun part1() = solve { it }
+    override fun part2() = solve { it * (it + 1) / 2 }
 
-    override fun part2(): Any? {
-        return rangeToCheck.minOf { candidate ->
-            parsed.map { it - candidate }.map(Int::absoluteValue).sumOf { it * (it + 1) / 2 }
-        }
+    private fun solve(mapping: (Int) -> Int) = rangeToCheck.minOf { candidate ->
+        parsed.map { abs(it - candidate) }.sumOf(mapping)
     }
 }
