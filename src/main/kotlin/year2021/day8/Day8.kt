b628e76ff5f4bff4473c65b1b03278770dfaca9b
Michael de Kaste <michael.de.kaste@topicus.nl>
2021-12-08 15:26:30 +0100

    small cleanup

--- src/main/kotlin/year2021/day8/Day8.kt
+++ src/main/kotlin/year2021/day8/Day8.kt
@@ -5,13 +5,11 @@ import Challenge
 fun main() = Day8.printSolutions()
 
 object Day8 : Challenge() {
-    val parsed = input
-        .lines()
-        .map { line -> line.split(" | ").map { wires -> wires.split(" ").map { it.toSortedSet() } } }
+    val parsed = input.lines().map { it.split(" | ").map { it.split(" ").map { it.toSortedSet() } } }
 
-    override fun part1() = parsed.sumOf { it[1].map { it.size }.count(listOf(2, 4, 3, 7)::contains) }
+    override fun part1() = parsed.sumOf { (_, output) -> output.map(Set<*>::size).count(listOf(2, 4, 3, 7)::contains) }
     override fun part2() = parsed.sumOf { (input, output) ->
-        Array<Set<Char>>(10) { emptySet() }.apply {
+        val wires = Array<Set<Char>>(10) { emptySet() }.apply {
             this[1] = input.single { it.size == 2 }
             this[4] = input.single { it.size == 4 }
             this[7] = input.single { it.size == 3 }
@@ -22,7 +20,8 @@ object Day8 : Challenge() {
             this[9] = input.single { it.size == 6 && this[3] in it }
             this[6] = input.single { it.size == 6 && this[8] - this[1] in it }
             this[0] = input.single { it.size == 6 && this[5] !in it }
-        }.let { p -> output.fold(0L) { int, set -> int * 10 + p.indexOf(set) } }
+        }
+        output.fold(0L) { int, set -> int * 10 + wires.indexOf(set) }
     }
 
     operator fun <T> Set<T>.contains(other: Set<T>) = containsAll(other)
