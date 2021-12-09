0ac2fc8266eaa088b323728d5dd3fce97709a258
Michael de Kaste <michael.de.kaste@topicus.nl>
2021-12-09 11:44:45 +0100

    singlelined most stuff, cant decide whats nicer

--- src/main/kotlin/year2021/day9/Day9.kt
+++ src/main/kotlin/year2021/day9/Day9.kt
@@ -5,23 +5,15 @@ import Challenge
 fun main() = Day9.printMeasure()
 
 object Day9 : Challenge() {
-    val parsed = input.lines().withIndex().flatMap { (y, line) ->
+    private val parsed = input.lines().withIndex().flatMap { (y, line) ->
         line.withIndex().map { (x, char) ->
             Tile(y, x, char.digitToInt())
         }
     }.associateBy { (y, x, _) -> y to x }
+    private val localMinima = parsed.values.filter(Tile::isLocalMinima)
 
-    private val localMinima = parsed
-        .values
-        .filter(Tile::isLocalMinima)
-
-    override fun part1() = localMinima
-        .sumOf { it.height + 1 }
-
-    override fun part2() = localMinima
-        .map(Tile::sizeOfBasin)
-        .sortedDescending()
-        .let { (a, b, c) -> a * b * c }
+    override fun part1() = localMinima.sumOf { it.height + 1 }
+    override fun part2() = localMinima.map(Tile::sizeOfBasin).sortedDescending().let { (a, b, c) -> a * b * c }
 
     data class Tile(val y: Int, val x: Int, val height: Int) {
         fun neighbours() = listOf(y - 1 to x, y + 1 to x, y to x - 1, y to x + 1).mapNotNull(parsed::get)
