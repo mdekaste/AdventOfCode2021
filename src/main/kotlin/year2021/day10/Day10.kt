1dc044f73db4a913ec4ebc6261f7fc8d4b8df20f
Michael de Kaste <michael.de.kaste@topicus.nl>
2021-12-10 15:20:34 +0100

    got rid of the type

--- src/main/kotlin/year2021/day10/Day10.kt
+++ src/main/kotlin/year2021/day10/Day10.kt
@@ -3,45 +3,46 @@ package year2021.day10
 import Challenge
 import kotlin.math.max
 
-fun main() = Day10.printSolutions()
+fun main() = Day10.printMeasure()
 
 object Day10 : Challenge() {
-    val brackets = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
+    private val brackets = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
 
-    sealed interface Type {
-        @JvmInline value class Incomplete(val list: List<Char>) : Type
-        @JvmInline value class Illegal(val char: Char) : Type
-    }
+    private val illegal = mutableListOf<Char>()
+    private val incomplete = mutableListOf<List<Char>>()
 
-    val parsed = input.lines().map { line ->
-        val reducing = line.toMutableList()
-        var index = 0
-        while (index < reducing.size - 1) {
-            when (val next = reducing[index + 1]) {
-                in brackets.keys -> index++
-                brackets[reducing[index]] -> {
-                    reducing.removeAt(index)
-                    reducing.removeAt(index)
-                    index = max(index - 1, 0)
+    init {
+        loop@for (line in input.lines()) {
+            val reducing = line.toMutableList()
+            var index = 0
+            while (index < reducing.size - 1) {
+                when (val next = reducing[index + 1]) {
+                    in brackets.keys -> index++
+                    brackets[reducing[index]] -> {
+                        reducing.removeAt(index)
+                        reducing.removeAt(index)
+                        index = max(index - 1, 0)
+                    }
+                    else -> {
+                        illegal.add(next)
+                        continue@loop
+                    }
                 }
-                else -> return@map Type.Illegal(next)
             }
+            incomplete.add(reducing)
         }
-        Type.Incomplete(reducing)
     }
 
-    override fun part1() = parsed.filterIsInstance(Type.Illegal::class.java)
-        .map(Type.Illegal::char)
+    override fun part1() = illegal
         .map(brackets.values::indexOf)
         .sumOf(listOf(3, 57, 1197, 25137)::get)
 
-    override fun part2() = parsed.filterIsInstance(Type.Incomplete::class.java)
-        .map(Type.Incomplete::list)
+    override fun part2() = incomplete
         .map(::getScore)
         .sorted()
         .let { it[it.size / 2] }
 
-    fun getScore(list: List<Char>) = list.reversed()
+    private fun getScore(list: List<Char>) = list.reversed()
         .map(brackets.keys::indexOf)
         .fold(0L) { score, index -> score * 5 + index + 1 }
 }
