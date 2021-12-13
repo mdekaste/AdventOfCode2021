4d7f7c5ba0fe59b8fbc740393cfbf362711f5566
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2021-12-13 08:00:12 +0100

    Day13 cleanup

--- src/main/kotlin/year2021/day13/Day13.kt
+++ src/main/kotlin/year2021/day13/Day13.kt
@@ -1,62 +1,45 @@
 package year2021.day13
 
 import Challenge
+import kotlin.math.abs
+import kotlin.math.absoluteValue
 
 fun main() = Day13.printSolutions()
 
 object Day13 : Challenge() {
-    val parsed = input.split("\r\n\r\n").let { (first, second) ->
-        first.lines().map { it.split(",").map(String::toInt).let { (x,y) -> y to x } }to
-                second.lines().map { it.substringAfter("fold along ").split("=").let { (coord, number) ->
-                    coord to number.toInt()
-                } } }
-
-    override fun part1(): Any? {
-        val visible = parsed.first.toMutableSet()
-        fold(visible, parsed.second.first())
-        return visible.count()
-    }
-
-    fun fold(set: MutableSet<Pair<Int, Int>>, instr: Pair<String, Int>){
-        when(instr.first){
-            "x" -> {
-                val yHeight = set.maxOf { it.first }
-                for(y in 0..yHeight){
-                    set.remove(y to instr.second)
-                }
-                for(index in 1..instr.second){
-                    for(y in 0..yHeight){
-                        if(set.remove(y to instr.second + index))
-                            set.add(y to instr.second - index)
+    val LS = System.lineSeparator()
+    val parsed = input.split(LS + LS).let { (first, second) ->
+        first.lines().map { it.split(",").map(String::toInt).let { (x, y) -> Point(x, y) } }.toSet() to
+                second.lines().map {
+                    it.substringAfter("fold along ").split("=").let { (coord, number) ->
+                        when (coord) {
+                            "x" -> Fold(number.toInt(), hor = true)
+                            else -> Fold(number.toInt(), hor = false)
+                        }
                     }
                 }
-            }
-            "y" -> {
-                val xHeight = set.maxOf { it.second }
-                for(x in 0..xHeight){
-                    set.remove(instr.second to x)
-                }
-                for(index in 1..instr.second){
-                    for(x in 0..xHeight){
-                        if(set.remove(instr.second + index to x))
-                            set.add(instr.second - index to x)
-                    }
-                }
-            }
-        }
     }
 
-    override fun part2(): Any? {
-        val visible = parsed.first.toMutableSet()
-        for(fold in parsed.second){
-            fold(visible, fold)
-        }
-        for(y in 0..visible.maxOf { it.first }){
-            for(x in 0..visible.maxOf { it.second }){
-                print(if (visible.contains(y to x)) "#" else ".")
+    override fun part1() = folds(parsed.second, parsed.first)[1].size
+
+    override fun part2() = folds(parsed.second, parsed.first).last().let { points ->
+        (0..points.maxOf(Point::y)).map { y ->
+            (0..points.maxOf(Point::x)).joinToString("") { x ->
+                if (Point(x, y) in points) "#" else "."
             }
-            println()
-        }
-        return null
+        }.joinToString(LS, prefix = LS, postfix = LS)
+    }
+
+    private fun folds(folds: List<Fold>, points: Set<Point>) = folds.runningFold(points) { list, fold ->
+        list.mapNotNullTo(mutableSetOf()) { it.foldOver(fold) }
     }
 }
+
+data class Point(val x: Int, val y: Int) {
+    fun foldOver(fold: Fold): Point? = when (fold.hor) {
+        true -> if (x == fold.index) null else Point(fold.index - abs(fold.index - x), y)
+        false -> if (y == fold.index) null else Point(x, fold.index - abs(fold.index - y))
+    }
+}
+
+class Fold(val index: Int, val hor: Boolean)
